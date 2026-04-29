ad7cfa993893683147ee87148dabb63694dbc3e2
brycewc <bryce.cindrich@gmail.com>
2026-04-29 12:04:43 -0600

    New JSDoc that aligns with implementation for Domo Toolkit

--- main.js
+++ main.js
@@ -2,15 +2,29 @@
 
 const codeengine = require('codeengine');
 
+/**
+ * @typedef {object} User
+ * @property {number} id
+ * @property {text} displayName
+ * @property {text} userName
+ * @property {text} emailAddress
+ * @property {number} modified
+ * @property {number} created
+ * @property {number} roleId
+ * @property {boolean} isSystemUser
+ * @property {boolean} isActive
+ */
+
 class Helpers {
 	/**
-	 * Helper function to handle API requests and errors
-	 *
-	 * @param {string} method - The HTTP method
-	 * @param {string} url - The endpoint URL
+	 * @private
+	 * @summary Handle Request
+	 * @description Helper function to handle API requests and errors
+	 * @param {text} method - The HTTP method
+	 * @param {text} url - The endpoint URL
 	 * @param {object} [body=null] - The request body
 	 * @param {object} [headers=null] - The request headers
-	 * @param {string} [content='application/json'] - Request body content type
+	 * @param {text} [contentType='application/json'] - Request body content type
 	 * @returns {object} The response data
 	 * @throws {error} If the request fails
 	 */
@@ -30,9 +44,9 @@ class Helpers {
 const { handleRequest } = Helpers;
 
 /**
- * Generates a Universally Unique Identifier (UUID)
- *
- * @returns {string} uuid
+ * @summary Generate UUID
+ * @description Generates a Universally Unique Identifier (UUID)
+ * @returns {text} uuid
  */
 function generateUUID() {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -43,45 +57,44 @@ function generateUUID() {
 }
 
 /**
- * Determine the length of the provided list
- *
- * @param {integer[]} list - The list to get the length of
- * @returns {integer} - The length of the list
+ * @summary Get List of Numbers Length
+ * @description Determine the length of the provided list
+ * @param {number[]} list - The list to get the length of
+ * @returns {number} The length of the list
  */
 function getListOfNumbersLength(list) {
 	return list.length;
 }
 
 /**
- * Retrieve the number at the specified index in a list
- *
- * @param {integer[]} list - The list of numbers to source from
- * @param {integer} index - The index of the number to get
- * @returns {integer} - The number at the specified index
+ * @summary Get Number from List
+ * @description Retrieve the number at the specified index in a list
+ * @param {number[]} list - The list of numbers to source from
+ * @param {number} index - The index of the number to get
+ * @returns {number} The number at the specified index
  */
 function getNumberFromList(list, index) {
 	return list[index];
 }
 
 /**
- * Takes an Epoch timestamp as a number and converts it to datetime
- *
- * @param {integer} epoch - The Epoch timestamp to cast, sent as a number
- * @returns {datetime} - The number at the specified index
+ * @summary Cast Epoch Timestamp Number as Datetime
+ * @description Takes an Epoch timestamp as a number and converts it to datetime
+ * @param {number} epoch - The Epoch timestamp to cast, sent as a number
+ * @returns {datetime} datetime - The resulting datetime after conversion
  */
 function castEpochTimestampNumberAsDatetime(epoch) {
 	return new Date(epoch);
 }
 
 /**
- * Shares a dataset with a person
- *
- * @param {Dataset} dataset - The dataset
- * @param {Person} person - The person to share the dataset with
- * @param {string} permission - The permission level to share the dataset with (default: 'CAN_SHARE')
- * @param {string} message - The message to include in the share email (default: 'I thought you might find this dataset interesting.')
- * @param {boolean} sendEmail - Whether to send an email notification to the person (default: false)
- * @returns {null}
+ * @summary Share DataSet with Person
+ * @description Shares a dataset with a person
+ * @param {dataset} dataset - The dataset
+ * @param {person} person - The person to share the dataset with
+ * @param {text} [permission='CAN_SHARE'] - The permission level to share the dataset with
+ * @param {text} [message='I thought you might find this dataset interesting.'] - The message to include in the share email
+ * @param {boolean} [sendEmail=false] - Whether to send an email notification to the person
  */
 async function shareDatasetWithPerson(
 	dataset,
@@ -103,9 +116,9 @@ async function shareDatasetWithPerson(
 }
 
 /**
- * Deletes all cards on a given page then deletes the page
- *
- * @param {string} pageId - integer id of page to delete
+ * @summary Delete Page and Cards
+ * @description Deletes all cards on a given page then deletes the page
+ * @param {text} pageId - integer id of page to delete
  * @returns {boolean} result - true if successful
  */
 async function deletePageAndCards(pageId) {
@@ -121,29 +134,27 @@ async function deletePageAndCards(pageId) {
 }
 
 /**
- * Deletes/revokes an API access token by ID
- *
- * @param {integer} accessTokenId - ID of the access token
- * @returns {null}
+ * @summary Delete Access Token
+ * @description Deletes/revokes an API access token by ID
+ * @param {number} accessTokenId - ID of the access token
  */
 async function deleteAccessToken(accessTokenId) {
 	await handleRequest('DELETE', `api/data/v1/accesstokens/${accessTokenId}`);
 }
 
 /**
- * Updates users in bulk
- *
+ * @summary Bulk Update Users
+ * @description Updates users in bulk
  * @param {object[]} users
- * 	Properties:
- * 	- id {string}
- *  - displayName {string}
- *  - title {string}
- *  - department {string}
- *  - employeeId {string}
- *  - employeeNumber {string}
- *  - hireDate {integer}
- *  - reportsTo {string}
- *  - phoneNumber {string}
+ * @param {text} users[].id
+ * @param {text} [users[].displayName]
+ * @param {text} [users[].title]
+ * @param {text} [users[].department]
+ * @param {text} [users[].employeeId]
+ * @param {text} [users[].employeeNumber]
+ * @param {number} [users[].hireDate]
+ * @param {text} [users[].reportsTo]
+ * @param {text} [users[].phoneNumber]
  */
 async function bulkUpdateUsers(users) {
 	for (const user of users) {
@@ -157,10 +168,10 @@ async function bulkUpdateUsers(users) {
 }
 
 /**
- * Updates reportsTo field (manager) of a user
- *
- * @param {integer} userId - ID of the user to update
- * @param {integer} managerId - ID of the manager user to set as reportsTo
+ * @summary Update Manager
+ * @description Updates reportsTo field (manager) of a user
+ * @param {number} userId - ID of the user to update
+ * @param {number} managerId - ID of the manager user to set as reportsTo
  */
 async function updateManager(userId, managerId) {
 	const url = `/api/content/v2/users/${userId}/teams`;
@@ -169,9 +180,12 @@ async function updateManager(userId, managerId) {
 }
 
 /**
- * Updates specified attributes for a user
- * @param {integer} userId - ID of the user to update
+ * @summary Update User Attributes
+ * @description Updates specified attributes for a user
+ * @param {number} userId - ID of the user to update
  * @param {object[]} attributes - An array of attribute objects, with key and values properties
+ * @param {text} attributes[].key
+ * @param {text[]} attributes[].values
  */
 async function updateUserAttributes(userId, attributes) {
 	await handleRequest('PATCH', `/api/identity/v1/users/${userId}`, {
@@ -180,18 +194,19 @@ async function updateUserAttributes(userId, attributes) {
 }
 
 /**
- * Updates roles for multiple users
- * @param {Person[]} people - The people
- * @param {integer} roleId - The new role
+ * @summary Bulk Update User Roles
+ * @description Updates roles for multiple users
+ * @param {person[]} people - The people
+ * @param {number} roleId - The new role
  */
 async function bulkUpdateUserRoles(people, roleId) {
 	await handleRequest('PUT', `/api/authorization/v1/roles/${roleId}/users`, people);
 }
 
 /**
- * Get users that have a grant (or grants by comma separated values)
- *
- * @param {string} grant - grant or grants to search for
+ * @summary Get Users by Grant
+ * @description Get users that have a grant (or grants by comma separated values)
+ * @param {text} grant - grant or grants to search for
  * @returns {object[]} users - Array of users that have that grant
  */
 async function getUsersByGrant(grant) {
@@ -224,9 +239,9 @@ async function getUsersByGrant(grant) {
 }
 
 /**
- * Gets members of a group
- *
- * @param {integer} groupId - ID of the group
+ * @summary Get Group Members
+ * @description Gets members of a group
+ * @param {number} groupId - ID of the group
  * @returns {object[]} members - Array of users in the group
  */
 async function getGroupMembers(groupId) {
@@ -236,12 +251,13 @@ async function getGroupMembers(groupId) {
 }
 
 /**
- * Updates members of a group
- *
- * @param {integer} groupId - ID of the group
+ * @summary Update Group Members
+ * @description Updates members of a group
+ * @param {number} groupId - ID of the group
  * @param {object[]} addMembers - Array of users to add
- * @param {object[]} removeMembers- Array of users to remove
- * @returns {null}
+ * @param {text} addMembers[].id - The user ID
+ * @param {object[]} removeMembers - Array of users to remove
+ * @param {text} removeMembers[].id - The user ID
  */
 async function updateGroupMembers(groupId, addMembers, removeMembers) {
 	// Ensure both arrays have the correct structure
@@ -266,29 +282,35 @@ async function updateGroupMembers(groupId, addMembers, removeMembers) {
 }
 
 /**
- * Search for users
- *
+ * @summary Convert Group to Dynamic
+ * @description Converts a group to a dynamic group with a single attribute-based rule
+ * @param {number} id - ID of the group to convert
+ * @param {text} value - Attribute value to match for membership
+ * @param {text} [key='department'] - Attribute key to match on
+ */
+async function convertGroupToDynamic(id, value, key = 'department') {
+	const body = {
+		id,
+		type: 'dynamic',
+		dynamicDefinition: {
+			expression: {
+				operator: 'AND',
+				operands: [{ key, value }]
+			}
+		}
+	};
+	await handleRequest('PUT', `/api/content/v2/groups/${id}`, body);
+}
+
+/**
+ * @summary Search Users
+ * @description Search for users
  * @param {object} query - The query to search for
- * Example:
- * {
- * "field": "reportsTo",
- * "values": [
- * "123456"
- * ],
- * "operator": "EQ",
- * "filterType": "value"
- * }
- * @returns {object[]} users - Array of users that match the query
- * Properties:
- * 	- id {integer}
- *  - displayName {string}
- *  - userName {string}
- *  - emailAddress {string}
- *  - modified {integer}
- *  - created {integer}
- *  - roleId {integer}
- *  - isSystemUser {boolean}
- *  - isActive {boolean}
+ * @param {text} query.field - Field to filter on (e.g., reportsTo)
+ * @param {text[]} query.values - Values to filter on
+ * @param {text} query.operator - Filter operator (e.g., EQ)
+ * @param {text} query.filterType - Filter type (e.g., value)
+ * @returns {User[]} users - Array of users that match the query
  */
 async function searchUsers(query) {
 	const limit = 100;
@@ -344,20 +366,10 @@ async function searchUsers(query) {
 }
 
 /**
- * Get a user object from a person object
- *
- * @param {Person} person - The person
- * @returns {object} user - Information about the person
- * 	Properties:
- * 	- id {integer}
- *  - displayName {string}
- *  - userName {string}
- *  - emailAddress {string}
- *  - modified {integer}
- *  - created {integer}
- *  - roleId {integer}
- *  - isSystemUser {boolean}
- *  - isActive {boolean}
+ * @summary Get Person
+ * @description Get a user object from a person object
+ * @param {person} person - The person
+ * @returns {User} user - Information about the person
  */
 async function getPerson(person) {
 	const response = await handleRequest('GET', `api/identity/v1/users/${person}?parts=detailed`);
@@ -383,58 +395,59 @@ async function getPerson(person) {
 }
 
 /**
- * Casts a string User ID to a person object
- *
- * @param {string} userId - ID of the user
- * @returns {Person} person - Person object
+ * @summary Cast User ID to Person
+ * @description Casts a string User ID to a person object
+ * @param {text} userId - ID of the user
+ * @returns {person} person - Person object
  */
 async function castUserIdToPerson(userId) {
 	return userId;
 }
 
 /**
- * Casts an integer User ID to a person object
- *
- * @param {integer} userId - ID of the user
- * @returns {Person} person - Person object
+ * @summary Cast User ID Number to Person
+ * @description Casts an integer User ID to a person object
+ * @param {number} userId - ID of the user
+ * @returns {person} person - Person object
  */
 async function castUserIdNumToPerson(userId) {
 	return userId.toString();
 }
 
 /**
- * Casts an array of integer User IDs to an array of person objects
- *
- * @param {string[]} userIds - IDs of the users
- * @returns {Person[]} persons - Array of person objects
+ * @summary Cast User ID List to Person List
+ * @description Casts an array of text User IDs to an array of person objects
+ * @param {text[]} userIds - IDs of the users
+ * @returns {person[]} persons - Array of person objects
  */
 async function castUserIdListToPersonList(userIds) {
 	return userIds;
 }
 
 /**
- * Casts an array of integer User IDs to an array of person objects
- *
- * @param {integer[]} userIds - IDs of the users
- * @returns {Person[]} persons - Array of person objects
+ * @summary Cast User ID Number List to Person List
+ * @description Casts an array of integer User IDs to an array of person objects
+ * @param {number[]} userIds - IDs of the users
+ * @returns {person[]} persons - Array of person objects
  */
 async function castUserIdNumListToPersonList(userIds) {
 	return userIds.map(String);
 }
 
 /**
- * Concatenates a list of numbers into a text string separated by the specified separator
- *
- * @param {integer[]} list - Array of integers
- * @returns {string} concatenatedList - Concatenated string of integers
+ * @summary Concat Number List
+ * @description Concatenates a list of numbers into a text string separated by the specified separator
+ * @param {number[]} list - Array of integers
+ * @param {text} [separator=','] - Separator to use between numbers
+ * @returns {text} concatenatedList - Concatenated string of integers
  */
 async function concatNumList(list, separator = ',') {
 	return list.join(separator);
 }
 
 /**
- * Appends an object to an array of objects
- *
+ * @summary Add Object to List
+ * @description Appends an object to an array of objects
  * @param {object} object - Object to append
  * @param {object[]} list - Array of objects to append to
  * @returns {object[]} newList - Resulting array of objects
@@ -448,11 +461,11 @@ async function addObjectToList(object, list = []) {
 }
 
 /**
- * Appends a string to an array of strings
- *
- * @param {string} string - String to append
- * @param {string[]} list - Array of strings to append to
- * @returns {string[]} newList - Resulting array of strings
+ * @summary Add String to List
+ * @description Appends a string to an array of strings
+ * @param {text} string - String to append
+ * @param {text[]} list - Array of strings to append to
+ * @returns {text[]} newList - Resulting array of strings
  */
 async function addStringToList(string, list = []) {
 	if (list.length) {
@@ -463,8 +476,8 @@ async function addStringToList(string, list = []) {
 }
 
 /**
- * Checks if an object is empty
- *
+ * @summary Check Empty Object
+ * @description Checks if an object is empty
  * @param {object} obj - Object to check
  * @returns {boolean} empty - Whether the obj is empty or not
  */
@@ -473,9 +486,9 @@ function checkEmptyObject(obj = {}) {
 }
 
 /**
- * Returns account properties with secrets exposed
- *
- * @param {Account} account - Account to return properties from
+ * @summary Read Account Credentials
+ * @description Returns account properties with secrets exposed
+ * @param {account} account - Account to return properties from
  * @returns {object} result - Account properties
  */
 async function readAccountCredentials(account) {
